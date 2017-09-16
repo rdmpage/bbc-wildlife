a7280640dd269d10f8869463b03d6a3a26b68302
Roderic Page <r.page@bio.gla.ac.uk>
2017-09-16 11:45:43 +0100

    Generate triples

---
+++ code/merge.php
@@ -0,0 +1,69 @@
+<?php
+
+// Merge RDF/XML files into single n-triples file
+
+require_once(dirname(__FILE__) . '/arc2/ARC2.php');
+
+
+//--------------------------------------------------------------------------------------------------
+function rdf_to_triples($xml)
+{	
+	// Parse RDF into triples
+	$parser = ARC2::getRDFParser();		
+	$base = 'http://www.bbc.co.uk';
+	$parser->parse($base, $xml);	
+	
+	$triples = $parser->getTriples();
+	
+	//print_r($triples);
+	
+	// clean up
+	
+	$cleaned_triples = array();
+	foreach ($triples as $triple)
+	{
+		$add = true;
+
+		if ($triple['s'] == 'http://example.com/')
+		{
+			$add = false;
+		}
+		
+		if ($add)
+		{
+			$cleaned_triples[] = $triple;
+		}
+	}
+	
+	return $parser->toNTriples($cleaned_triples);
+	
+
+}
+
+
+
+//$basedir = dirname(dirname(__FILE__)) . '/ecozones';
+//$basedir = dirname(dirname(__FILE__)) . '/adaptations';
+//$basedir = dirname(dirname(__FILE__)) . '/habitats';
+$basedir = dirname(dirname(__FILE__)) . '/life';
+
+$files = scandir($basedir);
+
+foreach ($files as $filename)
+{
+	//echo "filename=$filename|\n";
+	
+	if (preg_match('/\.rdf$/', $filename))
+	{	
+		$rdf = file_get_contents($basedir . '/' . $filename);
+
+		// convert to triples
+		$triples = rdf_to_triples($rdf);
+
+		echo $triples;
+
+	}
+}
+
+?>
+
